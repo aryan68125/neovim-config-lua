0a92fe65c93b86191341737c96221ca3f9c5c22a
aditya <aryan68125@gmail.com>
2025-03-30 14:49:20 +0530

    Updated the plugins of Gitsigns

--- nvchad_neovim_setup_modified/lua/plugins/gitsigns.lua
+++ nvchad_neovim_setup_modified/lua/plugins/gitsigns.lua
@@ -4,5 +4,6 @@ return {
     require("gitsigns").setup()
     vim.keymap.set("n","<leader>gp",":Gitsigns preview_hunk<CR>",{})
     vim.keymap.set("n","<leader>clb",":Gitsigns toggle_current_line_blame<CR>",{})
+    vim.keymap.set("n","<leader>diff",":Gitsigns toggle_word_diff<CR>",{})
   end
 }
