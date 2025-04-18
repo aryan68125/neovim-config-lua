b078d6c0d206b7f519d939c4275f031317df2a70
aryan68125 <aryan68125@gmail.com>
2025-04-18 08:13:02 +0530

    Added a comment warning about what method of key bindings works and what method doesn't in case of nvchad setup for neovim

--- nvchad_neovim_setup_modified/lua/plugins/lazygit.lua
+++ nvchad_neovim_setup_modified/lua/plugins/lazygit.lua
@@ -1,6 +1,7 @@
 return {
     "kdheepak/lazygit.nvim",
     lazy = true,
+    -- Innvchad environemnt for neovim this method of setting up key binding only work
     config = function()
       require("gitsigns").setup()
       vim.keymap.set("n","<leader>lg",":LazyGit",{})
@@ -23,6 +24,7 @@ return {
     },
     -- setting the keybinding for LazyGit with 'keys' is recommended in
     -- order to load the plugin when the command is run for the first time
+    -- This method of key bindings did not work for me in nvchad setup although this will work if you are setting up neovim from scratch
     keys = {
         -- { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
         { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit", mode = "n" }
